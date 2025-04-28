4fbe2e420ba98161448a1486608572a52cee0dd2
Flora <flora.mataix@gmail.com>
2025-04-28 16:02:56 +0200

    Add files via upload
    code qui mesure la distance grâce aux 8 capteurs US

---
+++ new1captUS.ino
@@ -0,0 +1,62 @@
+#include "Ultrasonic.h"
+
+// Définition des broches des capteurs
+const int numCapteurs = 8;  // Nombre de capteurs
+int triggerPins[numCapteurs] = {3,5,7,9,11,13,A3,A5};  // Broches Trigger
+int echoPins[numCapteurs] = {2,4,6,8,10,12,A2,A4};     // Broches Echo
+
+// Création d'un tableau de capteurs
+Ultrasonic capteurs[numCapteurs] = {
+  Ultrasonic(triggerPins[0], echoPins[0]),
+  Ultrasonic(triggerPins[1], echoPins[1]),
+  Ultrasonic(triggerPins[2], echoPins[2]),
+  Ultrasonic(triggerPins[3], echoPins[3]),
+  Ultrasonic(triggerPins[4], echoPins[4]),
+  Ultrasonic(triggerPins[5], echoPins[5]),
+  Ultrasonic(triggerPins[6], echoPins[6]),
+  Ultrasonic(triggerPins[7], echoPins[7])
+};
+void sendInfo(int distances[], size_t size){
+  Serial2.write((byte*)distances, size);
+};
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);   // Pour envoyer des logs dans le Serial Monitor (Arduino IDE - via USB)
+  Serial2.begin(9600);  // Pour envoyer des informations à l'autre arduino (TX2 port 16 ; RX2 port 17)
+}
+
+void loop() {
+
+  int distances[numCapteurs] ;
+  for (int i = 0; i < numCapteurs; i++) {
+    distances[i] = capteurs[i].read();
+  }
+
+
+  /*for (int i = 0; i < numCapteurs; i++) {
+    delay(50);
+    int distance = capteurs[i].read();
+    Serial.print("Capteur ");
+    Serial.print(i + 1 : );
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  Serial.println("-------------------");*/
+  delay(1000);
+ 
+  size_t sizedistances = numCapteurs;
+
+  for (int i = 0; i< sizedistances; i++) {
+    Serial.print("Capteur ");
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.print(distances[i]);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(1000);
+  sendInfo(distances, sizedistances);
+
+
+}
